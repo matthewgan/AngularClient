0c1c1186f5fdb8adb470db07f71575ec64611f11
matthewgan <matthewgan@126.com>
2019-03-13 19:26:49 +0800

    add shops and inventory for project use

---
+++ angular-client/src/app/shops/shops.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShopsComponent } from './shops.component';
+
+@NgModule({
+  declarations: [ShopsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ShopsModule { }
